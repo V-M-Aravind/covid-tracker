6f23a4409905067c720f621b3a0c5e871682e450
Aravind VM <502106@containerstore.com>
2023-06-21 21:51:11 +0530

    Updates

---
+++ assets/index-cff24d55.js
@@ -0,0 +1,1 @@
+import{j as n,B as r}from"./index-2d5edc10.js";const s="_container_c6orr_1",o={container:s},c=({retryMessage:e,onRetry:t=null})=>n.jsxs("div",{className:o.container,children:[n.jsx("p",{children:e}),t&&n.jsx(r,{variant:"contained",type:"submit",size:"large",onClick:t,children:"Retry"})]});export{c as R};
